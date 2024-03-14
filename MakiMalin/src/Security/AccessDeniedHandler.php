a1324aabb1c8a528a40e054339ee26b4fe0c7574
Jérémy Girard <oqaislenning@gmail.com>
2024-03-14 19:45:42 +0100

    Ajout check des roles

---
+++ MakiMalin/src/Security/AccessDeniedHandler.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function handle(Request $request, AccessDeniedException $accessDeniedException): ?Response
+    {
+        // Redirection vers la page d'accueil
+        return new RedirectResponse($this->router->generate('app_home'));
+    }
+}
+
+?>
