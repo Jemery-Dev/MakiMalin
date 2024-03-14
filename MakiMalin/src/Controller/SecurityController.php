4060191fbbc6623c80a4f240fdd9867bfa7c85f2
RAIMONDO AXEL p2201074 <118347630+Axelrmd@users.noreply.github.com>
2024-03-14 17:47:31 +0100

    ajout du login(ne fonctionne pas encore)

--- MakiMalin/src/Controller/SecurityController.php
+++ MakiMalin/src/Controller/SecurityController.php
@@ -5,32 +5,33 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use App\Repository\UtilisateurRepository;
 
 
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
-  class LoginController extends AbstractController
-  {
-      #[Route('/login', name: 'app_login')]
-      public function index(AuthenticationUtils $authenticationUtils, UtilisateurRepository $UtilisateurRepository): Response
-      {
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils, UtilisateurRepository $UtilisateurRepository): Response
+    {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
- 
+
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
- 
-        if($lastUsername != ""){
-            $user = $UtilisateurRepository->findOneBy(['username'=>'douze']);
-        }else{
-            $user = null;
-        }
+
+        $user = $UtilisateurRepository->findAll();
 
           return $this->render('login/index.html.twig', [
               'last_username' => $lastUsername,
               'error'         => $error,
               'user' => $user,
           ]);
-      }
-  }
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
